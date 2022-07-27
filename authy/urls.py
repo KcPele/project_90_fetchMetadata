9ff719184bac962370e2069baf7c8d76f4b56017
CoolTonie <nwachukwua99@gmail.com>
2022-07-27 01:06:57 +0100

    work on the password reset email in settings.py; include some inputs to urls.py in the authy app folder; some edits to misspellings in the templates, non-destructive editing

--- authy/urls.py
+++ authy/urls.py
@@ -1,8 +1,11 @@
+from re import template
+from tokenize import Name
+from unicodedata import name
 from django.urls import path, include
 from authy.views import UserProfile, EditProfile
 from django_registration.backends.activation.views import RegistrationView
 from .forms import AuthyRegistrationForm
-from django.contrib.auth import views as authViews 
+from django.contrib.auth import views as auth_views #changed from authViews to auth_views
 
 
 
@@ -14,5 +17,22 @@ urlpatterns = [
 	path("accounts/register/", RegistrationView.as_view(form_class=AuthyRegistrationForm), name="sdjango_registration_register"),
 	path("accounts/", include("django_registration.backends.activation.urls"))
 
+
+# ## Password reset as seen in django documentation
+	
+# 	# #submit email form
+# 	path('reset_password/', auth_views.PasswordResetView.as_view( template_name="authy/password_reset.html"),
+# 	name="reset_password"),
+	
+# 	## email sent success message
+# 	path('reset_password_done/', auth_views.PasswordResetDoneView.as_view( template_name="authy/password_reset_done.html"), 
+# 	name="password_reset_done"),
+	
+# 	##link to password reset sent to  email
+# 	path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view() name="password_reset_confirm"),
+	
+# 	##password successfully changed completed
+# 	path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="authy/password_reset_complete.html"),
+# 	name="password_reset_complete"),
 
 ]
